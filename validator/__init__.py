67dcec8cc4e2fd956fcefd0e67b5453c9678a3f6
ENDOH takanao <djmchl@gmail.com>
2017-12-25 15:23:28 +0700

    add email

--- validator/__init__.py
+++ validator/__init__.py
@@ -221,6 +221,49 @@ def Pattern(pattern):
     pattern_lambda.err_message = "must match regex pattern %s" % pattern
     return pattern_lambda
 
+from django.utils.encoding import force_text
+from django.core.validators import EmailValidator
+
+def Email():
+
+    user_regex = re.compile(
+        r"(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*$"  # dot-atom
+        r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-\011\013\014\016-\177])*"$)',  # quoted-string
+        re.IGNORECASE)
+    domain_regex = re.compile(
+        r'(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}|[A-Z0-9-]{2,})$'  # domain
+        # literal form, ipv4 address (SMTP 4.1.3)
+        r'|^\[(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\]$',
+        re.IGNORECASE)
+    domain_whitelist = ['localhost']
+
+    def email_lambda(value):
+        value = force_text(value)
+
+        if not value or '@' not in value:
+            return False
+
+        user_part, domain_part = value.rsplit('@', 1)
+
+        if not user_regex.match(user_part):
+            return False
+
+        if (not domain_part in domain_whitelist and
+                not domain_regex.match(domain_part)):
+            # Try for possible IDN domain-part
+            try:
+                domain_part = domain_part.encode('idna').decode('ascii')
+                if not domain_regex.match(domain_part):
+                    return False
+                else:
+                    return True
+            except UnicodeError:
+                pass
+            return False
+        return True
+
+    email_lambda.err_message = "enter a valid email address"
+    return email_lambda
 def validate(validation, dictionary):
     """
     Validate that a dictionary passes a set of
